53bf5b87e67651dcb47e29eeec7baa425961d7e1
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-06-19 17:41:33 -0600

    ⚡️perf: Create api endpoints

--- models/server.js
+++ models/server.js
@@ -18,8 +18,38 @@ class Server {
 
   routes() {
     this.app.get('/api', (req, res) => {
-      res.send('Hello World!');
-      res.end();
+      res.json({
+        ok: true,
+        msg: 'get API'
+      });
+    });
+
+    this.app.post('/api', (req, res) => {
+      res.json({
+        ok: true,
+        msg: 'post API'
+      });
+    });
+
+    this.app.put('/api', (req, res) => {
+      res.json({
+        ok: true,
+        msg: 'put API'
+      });
+    });
+
+    this.app.delete('/api', (req, res) => {
+      res.json({
+        ok: true,
+        msg: 'delete API'
+      });
+    });
+
+    this.app.patch('/api', (req, res) => {
+      res.json({
+        ok: true,
+        msg: 'patch API'
+      });
     });
   }
 
