d76240d77975bbd613a89cb9458cc21f13c9353d
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-06-22 23:12:49 -0600

    🆕 feat: Add google sign in and sign out

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs');
 
 const User = require('../models/user');
 const { generateJWT } = require('../helpers/generate-JWT');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req = request, res = response) => {
   const { email, password } = req.body;
@@ -50,6 +51,54 @@ const login = async (req = request, res = response) => {
   }
 }
 
+const googleSignIn = async (req = request, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    // Extract the google verified user
+    const { name, email, img } = await googleVerify(id_token);
+
+    // Search if the user exists in the DB
+    let user = await User.findOne({ email });
+
+    // If the user doesn't exist, create it
+    if (!user) {
+      const data = {
+        name,
+        email,
+        password: ':P',
+        img,
+        google: true
+      };
+
+      user = new User(data);
+      await user.save();
+    }
+
+    // Verify if the user is active in the DB
+    if (!user.status) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'User blocked, talk with the administrator'
+      });
+    }
+
+    // Generate the JWT
+    const token = await generateJWT(user.id);
+
+    res.json({
+      user,
+      token
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'Invalid token, could not verify'
+    });
+  }
+}
+
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
